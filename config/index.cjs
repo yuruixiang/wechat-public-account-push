598e8443a5ae7fe0f1dacf244cba9718f746560c
yuruixiang <124869076+yuruixiang@users.noreply.github.com>
2024-02-29 13:36:32 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o1HZz6UUv0qZqLUigndRRxXgX1FQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'irUm4oCq3EFefER0rX7_pnidmw6Zt_16_bWqj56xkLk',
+      useTemplateId: 'SrXTladxLKBh1rWMuHpHnBS5pd2vS3VVp38aqW4Z8R0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
