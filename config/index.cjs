d1b4fe10891d1db642f8f9501f72e46e65ba5446
yuruixiang <124869076+yuruixiang@users.noreply.github.com>
2024-02-29 14:08:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '大宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o1HZz6cIqNfRui66HvFfu67HLa6s',
+      id: 'o1HZz6R5jBj-KXA3IY6sNGzsP4do',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'SrXTladxLKBh1rWMuHpHnBS5pd2vS3VVp38aqW4Z8R0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
